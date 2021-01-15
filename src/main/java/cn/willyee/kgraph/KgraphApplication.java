7f46168299248445f42da2406e3e773e3e7afa3c
weiyuchen <weiyuchens@sina.com>
2021-01-15 13:40:07 +0800

    init project

---
+++ src/main/java/cn/willyee/kgraph/KgraphApplication.java
@@ -0,0 +1,13 @@
+package cn.willyee.kgraph;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KgraphApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KgraphApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cn/willyee/kgraph/KgraphApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.willyee.kgraph;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KgraphApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
